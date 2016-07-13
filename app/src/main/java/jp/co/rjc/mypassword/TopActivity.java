20c4a9fb707f96a2a269906ec1ee8c99c1c459e2
m_yoshida <m_yoshida@r-jc.jp>
2016-07-13 18:45:16 +0900

    #refs 9916～9926　レイアウト用xmlの作成

--- app/src/main/java/jp/co/rjc/mypassword/TopActivity.java
+++ app/src/main/java/jp/co/rjc/mypassword/TopActivity.java
@@ -9,12 +9,5 @@ public class TopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
-
-        //ToDo 新規登録画面に飛ぶ
-//        setContentView(R.layout.fragment_detail);
-        //ToDo 呼ぶ画面のIntentを設定する
-//        Intent intent = new Intent(this, TopActivity.class);
-//        intent.putExtra("foo", someData);
-//        startActivity(intent);
     }
 }
