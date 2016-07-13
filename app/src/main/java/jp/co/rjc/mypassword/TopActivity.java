b76f8af890264cbbe6c02dfc2b0ca66eea602600
t-nakahata <t_nakahata@RJC7052.r-jc.local>
2016-07-13 18:39:49 +0900

    refs #9909_【ログイン画面】取消(TextView) 9910、9911、9912のインスタンス生成も合わせて対応

--- app/src/main/java/jp/co/rjc/mypassword/TopActivity.java
+++ app/src/main/java/jp/co/rjc/mypassword/TopActivity.java
@@ -21,5 +21,31 @@ public class TopActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        // インスタンス生成
+        Button mCancel = (Button)findViewById(R.id.cancel);
+        TextView mInfo = (TextView)findViewById(R.id.info);
+        EditText mPin1 = (EditText)findViewById(R.id.pin1);
+        EditText mPin2 = (EditText)findViewById(R.id.pin2);
+        EditText mPin3 = (EditText)findViewById(R.id.pin3);
+        EditText mPin4 = (EditText)findViewById(R.id.pin4);
+        TextView mTenKey0 = (TextView)findViewById(R.id.tenKey0);
+        TextView mTenKey1 = (TextView)findViewById(R.id.tenKey1);
+        TextView mTenKey2 = (TextView)findViewById(R.id.tenKey2);
+        TextView mTenKey3 = (TextView)findViewById(R.id.tenKey3);
+        TextView mTenKey4 = (TextView)findViewById(R.id.tenKey4);
+        TextView mTenKey5 = (TextView)findViewById(R.id.tenKey5);
+        TextView mTenKey6 = (TextView)findViewById(R.id.tenKey6);
+        TextView mTenKey7 = (TextView)findViewById(R.id.tenKey7);
+        TextView mTenKey8 = (TextView)findViewById(R.id.tenKey8);
+        TextView mTenKey9 = (TextView)findViewById(R.id.tenKey9);
+        TextView mTenKeyBS = (TextView)findViewById(R.id.tenKeyBS);
+
+        // 取消ボタンの表示制御
+        Intent mI = getIntent();
+        boolean editFlg = mI.getBooleanExtra(“EDIT_FLG”);
+        if (editFlg) {
+            cancel.INVISIBLE;
+        }
     }
 }
